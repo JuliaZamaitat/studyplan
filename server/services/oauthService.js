3d7648f776d34f27ce31b5a7d290c8e729348fd8
JuliaZamaitat <j.zamaitat@gmail.com>
2021-07-09 15:32:24 +0200

    including production guard for oauth service.

--- server/services/oauthService.js
+++ server/services/oauthService.js
@@ -9,26 +9,28 @@ const refreshToken = process.env.OAUTH_REFRESH_TOKEN;
 
 const oauth2Client = new OAuth2(clientID, clientSecret, redirectURL);
 
-oauth2Client.setCredentials({
-  refresh_token: refreshToken,
-});
-const gmailAccessToken = oauth2Client.getAccessToken();
+if (process.env.PRODUCTION) {
+  oauth2Client.setCredentials({
+    refresh_token: refreshToken,
+  });
+  const gmailAccessToken = oauth2Client.getAccessToken();
 
-const gmailTransporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    type: "OAuth2",
-    user: "studyplanhtwberlin@gmail.com",
-    clientId: clientID,
-    clientSecret: clientSecret,
-    refreshToken: refreshToken,
-    accessToken: gmailAccessToken,
-    tls: {
-      rejectUnauthorized: false,
+  const gmailTransporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: "studyplanhtwberlin@gmail.com",
+      clientId: clientID,
+      clientSecret: clientSecret,
+      refreshToken: refreshToken,
+      accessToken: gmailAccessToken,
+      tls: {
+        rejectUnauthorized: false,
+      },
     },
-  },
-});
+  });
 
-module.exports = {
-  gmailTransporter: gmailTransporter,
-};
+  module.exports = {
+    gmailTransporter: gmailTransporter,
+  };
+}
