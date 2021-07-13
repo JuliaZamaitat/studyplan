d7c82792650297f094f8b3cff76d677060a3e34d
JuliaZamaitat <j.zamaitat@gmail.com>
2021-07-13 14:23:16 +0200

    adding explanation box for studyplan.

--- src/views/StudyPlan.vue
+++ src/views/StudyPlan.vue
@@ -7,6 +7,65 @@
       v-show="!pending"
       :coursesInSemester="this.studyPlan.semesterPlans"
     />
+
+    <div class="explanation">
+      <button
+        class="explanation explanation-button"
+        @click="showingExplanations = !showingExplanations"
+      >
+        <p v-if="showingExplanations">Hinweise verbergen</p>
+        <p v-else>Hinweise anzeigen</p>
+      </button>
+      <transition name="fade">
+        <div
+          v-if="showingExplanations"
+          class="explanation explanation-container"
+        >
+          <div class="explanatin explanation-questions">
+            <div class="explanation explanation-question">
+              <h4 class="explanation explanation-question--question">
+                Wie benutze ich den StudyPlan?
+              </h4>
+              <div class="explanation explanation-question--answers">
+                <p>
+                  Du kannst ganz einfach per Drag & Drop die Kurse zwischen den
+                  Semestern hin- und herziehen. Mit Klick auf einen Kurs kommst
+                  du zur Detailansicht. Du kannst einen Kurse als "belegt" und
+                  "bestanden" makieren. Manche Kurse müssen durch andere Kurse
+                  bestanden werden – du kannst dann nur dort "belegt" oder
+                  "bestanden" angeben.
+                </p>
+              </div>
+            </div>
+
+            <div class="explanation explanation-question">
+              <h4 class="explanation explanation-question--question">
+                Was bedeuten die Farben?
+              </h4>
+              <div class="explanation explanation-question--answers">
+                <p class="notBooked">noch nicht belegt</p>
+                <p class="booked">belegt</p>
+                <p class="passed">bestanden</p>
+              </div>
+            </div>
+            <div class="explanation explanation-question">
+              <h4 class="explanation explanation-question--question">
+                Disclaimer
+              </h4>
+              <div class="explanation explanation-question--answers">
+                <p>
+                  Das Tool dient deiner persönlichen Planung. Es überprüft
+                  (noch) nicht die Sinnhaftigkeit und Richtigkeit deiner
+                  Belegungspläne. Alle Angaben sind mit bestem Gewissen erstellt
+                  worden, was jedoch keine 100% Garantie für die Richtigkeit der
+                  Infos darstellt.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -18,9 +77,13 @@ export default {
     return {
       pending: {
         type: Boolean,
-        default: "false",
+        default: false,
       },
       color: "#76b900",
+      showingExplanations: {
+        type: Boolean,
+        default: false,
+      },
     };
   },
 
@@ -54,5 +117,90 @@ $htwGruen: #76b900;
 
 p {
   font-weight: bold;
+}
+
+.explanation {
+  margin-top: 30px;
+
+  .fade-enter-active {
+    transition: all 0.6s;
+  }
+  .fade-enter {
+    opacity: 0;
+  }
+
+  &-button {
+    background: none;
+    color: rgb(127, 127, 127);
+    border: none;
+    padding: 0;
+    font: inherit;
+    cursor: pointer;
+    outline: inherit;
+    text-decoration: underline;
+    font-weight: bold;
+    font-size: 18px;
+    margin-bottom: 20px;
+  }
+  &-container {
+    max-width: 1550px;
+    margin: 0 auto;
+    background: rgba(118, 185, 0, 0.12);
+    border: 1px solid rgba(118, 185, 0, 0.36);
+    border-radius: 12px;
+  }
+
+  &-questions {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin: auto;
+  }
+  &-question {
+    margin: 40px auto 40px;
+    width: 25%;
+
+    &--question {
+      margin: auto;
+    }
+    &--answers {
+      p {
+        display: inline;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 20px;
+      }
+
+      .passed {
+        color: $htwGruen;
+        display: block;
+      }
+      .booked {
+        color: rgba(253, 177, 62, 1);
+        display: block;
+      }
+      .notBooked {
+        color: rgb(127, 127, 127);
+        display: block;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 1600px) {
+  .explanation-container {
+    max-width: 90% !important;
+    margin: 0 auto;
+  }
+}
+
+@media screen and (max-width: 650px) {
+  .explanation-question {
+    width: 90%;
+  }
+  .explanation-questions {
+    flex-direction: column;
+  }
 }
 </style>
