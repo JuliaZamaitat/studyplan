5d09ade74d6030fa9d5f7be5296ab42930232b4a
JuliaZamaitat <j.zamaitat@gmail.com>
2021-07-15 12:47:27 +0200

    deployment wip.

--- main.js
+++ main.js
@@ -3,45 +3,16 @@ const express = require("express"),
   mongoose = require("mongoose"),
   cors = require("cors"),
   router = require("./routes/index"),
-  path = require("path");
-  serveStatic = require("serve-static"),
-   winston = require('winston');
+  // path = require("path");
+  serveStatic = require("serve-static");
 
-
-   const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
-    transports: [
-      //
-      // - Write all logs with level `error` and below to `error.log`
-      // - Write all logs with level `info` and below to `combined.log`
-      //
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' }),
-    ],
-  });
+  
 
 app.use(express.json());
 app.use(express.urlencoded());
 
 
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  logger.log({
-    level: 'info',
-    message: 'Intercepting'
-  });
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
+
 const mongodbURI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/studyplan";
 
@@ -60,7 +31,6 @@ app.use(
     extended: true,
   })
 );
-app.use(allowCrossDomain)
 app.use(express.json());
 // app.use(express.static("public")); //In order to use static file
 
@@ -73,12 +43,14 @@ app.set("view engine", "ejs"); //To use EJS
 
 
 // Serve static assets
-app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static(path.join(__dirname, 'dist')));
 
-// Redirect all requests to `index.html`
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-})
+// // Redirect all requests to `index.html`
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// })
+
+app.use(serveStatic(__dirname + '/dist'));
 
 
 app.use("/server", router);
