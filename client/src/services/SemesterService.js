0be15da39bb77a56fc7cc2da3d9b8a7877b5de81
JuliaZamaitat <j.zamaitat@gmail.com>
2021-07-15 10:26:07 +0200

    deploy wip

--- client/src/services/SemesterService.js
+++ client/src/services/SemesterService.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 import authHeader from "./AuthHeader";
+let url;
+if (process.env.PRODUCTION) {
+  url = "https://studyplan.herokuapp.com";
+} else {
+  url = "http://localhost:3000";
+}
 
 const server = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: url,
 });
 
 export default {
