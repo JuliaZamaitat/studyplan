89f4c93963bd1c660a2533e55b140f66400a0089
JuliaZamaitat <j.zamaitat@gmail.com>
2021-07-15 12:14:46 +0200

    deployment wip.

--- main.js
+++ main.js
@@ -4,7 +4,23 @@ const express = require("express"),
   cors = require("cors"),
   router = require("./routes/index"),
   path = require("path");
-  serveStatic = require("serve-static");
+  serveStatic = require("serve-static"),
+   winston = require('winston');
+
+
+   const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+      //
+      // - Write all logs with level `error` and below to `error.log`
+      // - Write all logs with level `info` and below to `combined.log`
+      //
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' }),
+    ],
+  });
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -14,7 +30,10 @@ var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  console.log("intercepting")
+  logger.log({
+    level: 'info',
+    message: 'Intercepting'
+  });
   // intercept OPTIONS method
   if ('OPTIONS' == req.method) {
     res.send(200);
